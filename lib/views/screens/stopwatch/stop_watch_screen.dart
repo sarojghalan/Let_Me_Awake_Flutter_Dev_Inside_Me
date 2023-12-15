0d7e43b638a9b13206e3f9daeca8670323c22aec
Saroj <jmestmnag@gmail.com>
2023-12-15 12:01:13 +0545

    `Target: Scroll Controller completed

--- lib/views/screens/stopwatch/stop_watch_screen.dart
+++ lib/views/screens/stopwatch/stop_watch_screen.dart
@@ -13,32 +13,27 @@ class StopWatch extends StatefulWidget {
 // it is the state associated with StopWatch widget
 class _StopWatchState extends State<StopWatch> {
   bool isTicking = false;
-  int seconds = 0;
+  int milliseconds = 0;
+  final laps = <int>[];
   late Timer timer;
+  final itemHeight = 60.0;
+  final scrollController = ScrollController();
 
   // defining single line function
   // in Dart we use arrow syntax or fat arrow syntax
   // to define a single line function
-  String _secondsText() => seconds == 1 ? 'second' : 'seconds';
-
-  @override
-  void initState() {
-    super.initState();
-    timer = Timer.periodic(const Duration(seconds: 1), _onTick);
-  }
-
-  @override
-  void dispose() {
-    timer.cancel();
-    super.dispose();
+  String _secondsText(int milliseconds) {
+    final seconds = milliseconds / 1000;
+    return '$seconds seconds';
   }
 
   // function to handle start time
   void _startTimer() {
-    timer = Timer.periodic(const Duration(seconds: 1), _onTick);
+    timer = Timer.periodic(const Duration(milliseconds: 100), _onTick);
     setState(() {
-      seconds = 0;
+      milliseconds = 0;
       isTicking = true;
+      laps.clear();
     });
   }
 
@@ -49,12 +44,29 @@ class _StopWatchState extends State<StopWatch> {
     });
   }
 
+  void _lap() {
+    setState(() {
+      laps.add(milliseconds);
+      milliseconds = 0;
+    });
+
+    scrollController.animateTo(itemHeight * laps.length,
+        duration: const Duration(milliseconds: 500), curve: Curves.easeIn);
+  }
+
   void _onTick(Timer time) {
     if (mounted) {
       setState(() {
-        ++seconds;
+        milliseconds += 100;
       });
     }
+  }
+
+  @override
+  void dispose() {
+    timer.cancel();
+    scrollController.dispose();
+    super.dispose();
   }
 
   @override
@@ -62,51 +74,94 @@ class _StopWatchState extends State<StopWatch> {
     return Scaffold(
         appBar: AppBar(
           title: const Text(
-            'State Ful Widgets',
+            'Stop Watch',
             style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
           ),
           centerTitle: true,
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '$seconds ${_secondsText()}',
-              style: Theme.of(context).textTheme.headlineSmall,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                ElevatedButton(
+        body: _buildContainer(context));
+  }
+
+  Container _buildContainer(BuildContext context) {
+    return Container(
+      color: Theme.of(context).primaryColor,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            '${laps.length + 1}',
+            style: Theme.of(context)
+                .textTheme
+                .headline5!
+                .copyWith(color: Colors.white),
+          ),
+          Text(
+            _secondsText(milliseconds),
+            style: Theme.of(context)
+                .textTheme
+                .headline5!
+                .copyWith(color: Colors.white),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () {
+                  isTicking ? null : _startTimer();
+                },
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.green),
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white)),
+                child: const Text('Start'),
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              TextButton(
                   onPressed: () {
-                    isTicking ? null : _startTimer();
+                    isTicking ? _stopTimer() : null;
                   },
                   style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(Colors.green),
                       foregroundColor:
-                          MaterialStateProperty.all<Color>(Colors.white)),
-                  child: const Text('Start'),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                TextButton(
-                    onPressed: () {
-                      isTicking ? _stopTimer() : null;
-                    },
-                    style: ButtonStyle(
-                        foregroundColor:
-                            MaterialStateProperty.all<Color>(Colors.white),
-                        backgroundColor:
-                            MaterialStateProperty.all<Color>(Colors.red)),
-                    child: const Text('Stop'))
-              ],
+                          MaterialStateProperty.all<Color>(Colors.white),
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.red)),
+                  child: const Text('Stop')),
+              const SizedBox(
+                width: 40,
+              ),
+              ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.yellow)),
+                  onPressed: isTicking ? _lap : null,
+                  child: const Text('Lap'))
+            ],
+          ),
+          Expanded(
+              child: Scrollbar(
+            controller: scrollController,
+            child: ListView.builder(
+              controller: scrollController,
+              itemExtent: itemHeight,
+              itemCount: laps.length,
+              itemBuilder: (context, index) {
+                final milliseconds = laps[index];
+                return ListTile(
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 50),
+                  title: Text('Lap ${index + 1}'),
+                  trailing: Text(_secondsText(milliseconds)),
+                );
+              },
             ),
-          ],
-        ));
+          ))
+        ],
+      ),
+    );
   }
 }
